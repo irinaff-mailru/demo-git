4dd26a7fc673599ebbc6b4b0ba47b6b4e5edfaca
Irina Fatkullina <irina.e.fatkullina@yandex.ru" irina.e.fatkullina@yandex.ru>
2025-12-07 20:30:29 +0500

    test commit 1

--- src/main/java/school/sorokin/demogit/demogit/UserService.java
+++ src/main/java/school/sorokin/demogit/demogit/UserService.java
@@ -1,4 +1,9 @@
 package school.sorokin.demogit.demogit;
 
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
 public class UserService {
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }
 }
