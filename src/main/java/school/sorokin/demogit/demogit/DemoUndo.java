169c915c7fb01c4c60f36bacf5fe70f3c8da5419
Irina Fatkullina <irina.e.fatkullina@yandex.ru" irina.e.fatkullina@yandex.ru>
2025-12-07 21:33:51 +0500

    test commit for many methods

--- src/main/java/school/sorokin/demogit/demogit/DemoUndo.java
+++ src/main/java/school/sorokin/demogit/demogit/DemoUndo.java
@@ -1,4 +1,18 @@
 package school.sorokin.demogit.demogit;
 
 public class DemoUndo {
+
+    public static void get1(String name) {
+    }
+    public static void get2(String name) {
+    }
+
+    public static void get3(String name) {
+    }
+
+    public static void get4(String name) {
+    }
+
+    public static void get5(String name) {
+    }
 }
